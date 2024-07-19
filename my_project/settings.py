3f6a634f75581a22291f4ee6028004c95ce823a7
Henry Bennet <hwbennett94@gmail.com>
2024-07-19 10:17:05 +0000

    Install gunicorn==20.1.0

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n640-)9g3016fy8m#i=%(**!f#pn@9!xv0)i2v3bi&@tger)0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-henrybennet-djangoproje-zienwqd3pgn.ws.codeinstitute-ide.net']
 
